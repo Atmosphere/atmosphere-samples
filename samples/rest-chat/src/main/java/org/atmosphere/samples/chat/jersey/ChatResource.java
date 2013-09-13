c6d726af2cb9bc48a1c80f5ff3ce36d7642058db
jfarcand <jfarcand@apache.org>
2013-09-13 14:44:04 -0400

    Add a listener

--- samples/rest-chat/src/main/java/org/atmosphere/samples/chat/jersey/ChatResource.java
+++ samples/rest-chat/src/main/java/org/atmosphere/samples/chat/jersey/ChatResource.java
@@ -18,6 +18,9 @@ package org.atmosphere.samples.chat.jersey;
 import org.atmosphere.annotation.Broadcast;
 import org.atmosphere.annotation.Suspend;
 import org.atmosphere.config.service.AtmosphereService;
+import org.atmosphere.cpr.AtmosphereResourceEvent;
+import org.atmosphere.cpr.AtmosphereResourceEventListenerAdapter;
+import org.atmosphere.jersey.JerseyBroadcaster;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -30,21 +33,24 @@ import javax.ws.rs.Produces;
  * @author Jeanfrancois Arcand
  */
 @Path("/")
-@AtmosphereService
+@AtmosphereService (broadcaster = JerseyBroadcaster.class)
 public class ChatResource {
 
     /**
      * Suspend the response without writing anything back to the client.
+     *
      * @return a white space
      */
-    @Suspend(contentType = "application/json")
+    @Suspend(contentType = "application/json", listeners = {OnDisconnect.class})
     @GET
     public String suspend() {
+
         return "";
     }
 
     /**
      * Broadcast the received message object to all suspended response. Do not write back the message to the calling connection.
+     *
      * @param message a {@link Message}
      * @return a {@link Response}
      */
@@ -53,6 +59,16 @@ public class ChatResource {
     @Produces("application/json")
     public Response broadcast(Message message) {
         return new Response(message.author, message.message);
+    }
+
+    public static final class OnDisconnect extends AtmosphereResourceEventListenerAdapter {
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void onDisconnect(AtmosphereResourceEvent event) {
+            System.out.println(event);
+        }
     }
 
 }
