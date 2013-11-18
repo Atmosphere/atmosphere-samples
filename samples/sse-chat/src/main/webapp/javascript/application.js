9c0768e45a4e1d96827d04a9b536b1d1aedd277b
jfarcand <jfarcand@apache.org>
2013-11-18 09:03:39 -0500

    Improve sample

--- samples/sse-chat/src/main/webapp/javascript/application.js
+++ samples/sse-chat/src/main/webapp/javascript/application.js
@@ -9,20 +9,28 @@ $(function () {
     var logged = false;
     var socket = atmosphere;
     var request = { url: document.location.toString() + 'chat',
-                    contentType : "application/json",
-                    logLevel : 'debug',
-                    transport : 'sse' ,
-                    fallbackTransport: 'long-polling'};
+        contentType: "application/json",
+        logLevel: 'debug',
+        transport: 'sse',
+        reconnectInterval: 5000,
+
+        fallbackTransport: 'long-polling'};
 
 
-    request.onOpen = function(response) {
+    request.onOpen = function (response) {
         content.html($('<p>', { text: 'Atmosphere connected using ' + response.transport }));
         input.removeAttr('disabled').focus();
         status.text('Choose name:');
     };
 
     request.onReconnect = function (request, response) {
-        socket.info("Reconnecting")
+        content.html($('<p>', { text: 'Connection lost, trying to reconnect. Trying to reconnect ' + request.reconnectInterval}));
+        input.attr('disabled', 'disabled');
+    };
+
+    request.onReopen = function (response) {
+        input.removeAttr('disabled').focus();
+        content.html($('<p>', { text: 'Atmosphere re-connected using ' + response.transport }));
     };
 
     request.onMessage = function (response) {
@@ -40,23 +48,24 @@ $(function () {
             status.text(myName + ': ').css('color', 'blue');
         } else {
             var me = json.author == author;
-            var date =  typeof(json.time) == 'string' ? parseInt(json.time) : json.time;
+            var date = typeof(json.time) == 'string' ? parseInt(json.time) : json.time;
             addMessage(json.author, json.text, me ? 'blue' : 'black', new Date(date));
         }
     };
 
-    request.onClose = function(response) {
-        logged = false;
-    }
+    request.onClose = function (response) {
+        content.html($('<p>', { text: 'Server closed the connection after a timeout' }));
+        input.attr('disabled', 'disabled');
+    };
 
-    request.onError = function(response) {
+    request.onError = function (response) {
         content.html($('<p>', { text: 'Sorry, but there\'s some problem with your '
             + 'socket or the server is down' }));
     };
 
     var subSocket = socket.subscribe(request);
 
-    input.keydown(function(e) {
+    input.keydown(function (e) {
         if (e.keyCode === 13) {
             var msg = $(this).val();
 
@@ -76,8 +85,7 @@ $(function () {
     });
 
     function addMessage(author, message, color, datetime) {
-        content.append('<p><span style="color:' + color + '">' + author + '</span> @ ' +
-            + (datetime.getHours() < 10 ? '0' + datetime.getHours() : datetime.getHours()) + ':'
+        content.append('<p><span style="color:' + color + '">' + author + '</span> @ ' + +(datetime.getHours() < 10 ? '0' + datetime.getHours() : datetime.getHours()) + ':'
             + (datetime.getMinutes() < 10 ? '0' + datetime.getMinutes() : datetime.getMinutes())
             + ': ' + message + '</p>');
     }
