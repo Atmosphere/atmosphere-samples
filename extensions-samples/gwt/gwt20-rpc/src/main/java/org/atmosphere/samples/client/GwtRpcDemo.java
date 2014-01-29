1a06ceb913f95ad67d7b73c4bf658f1d653dfc76
Kamal Bhatt <kamal.bhatt@gmail.com>
2014-01-29 11:46:37 +1100

    Fixed samples to work with new API as defined by https://github.com/Atmosphere/atmosphere-extensions/issues/127#issuecomment-33477178
    Also, modified the managed RPC web.xml to validate and modified projects to generate permuations for non-firefox browsers.

--- extensions-samples/gwt/gwt20-rpc/src/main/java/org/atmosphere/samples/client/GwtRpcDemo.java
+++ extensions-samples/gwt/gwt20-rpc/src/main/java/org/atmosphere/samples/client/GwtRpcDemo.java
@@ -15,6 +15,20 @@
  */
 package org.atmosphere.samples.client;
 
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.atmosphere.gwt20.client.Atmosphere;
+import org.atmosphere.gwt20.client.AtmosphereCloseHandler;
+import org.atmosphere.gwt20.client.AtmosphereMessageHandler;
+import org.atmosphere.gwt20.client.AtmosphereOpenHandler;
+import org.atmosphere.gwt20.client.AtmosphereReopenHandler;
+import org.atmosphere.gwt20.client.AtmosphereRequestConfig;
+import org.atmosphere.gwt20.client.AtmosphereServerRequest;
+import org.atmosphere.gwt20.client.AtmosphereServerResponse;
+import org.atmosphere.gwt20.client.RequestConfig;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -27,19 +41,6 @@ import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.atmosphere.gwt20.client.Atmosphere;
-import org.atmosphere.gwt20.client.AtmosphereCloseHandler;
-import org.atmosphere.gwt20.client.AtmosphereMessageHandler;
-import org.atmosphere.gwt20.client.AtmosphereOpenHandler;
-import org.atmosphere.gwt20.client.AtmosphereReopenHandler;
-import org.atmosphere.gwt20.client.AtmosphereRequest;
-import org.atmosphere.gwt20.client.AtmosphereRequestConfig;
-import org.atmosphere.gwt20.client.AtmosphereResponse;
-
-import org.atmosphere.gwt20.client.AtmosphereRequestConfig.Flags;
 
 /**
  *
@@ -48,29 +49,29 @@ import org.atmosphere.gwt20.client.AtmosphereRequestConfig.Flags;
 public class GwtRpcDemo implements EntryPoint {
 
     static final Logger logger = Logger.getLogger(GwtRpcDemo.class.getName());
-    
+
     @Override
     public void onModuleLoad() {
-        
+
         GWT.setUncaughtExceptionHandler(new GWT.UncaughtExceptionHandler() {
             @Override
             public void onUncaughtException(Throwable e) {
                 logger.log(Level.SEVERE, "Uncaught exception", e);
             }
         });
-        
-      
+
+
         HorizontalPanel buttons = new HorizontalPanel();
         final TextBox messageInput = new TextBox();
         buttons.add(messageInput);
-        
+
         Button sendRPC = new Button("send (GWT-RPC)");
         buttons.add(sendRPC);
-      
-                
+
+
         RootPanel.get("buttonbar").add(buttons);
-        
-        
+
+
         HTMLPanel logPanel = new HTMLPanel("") {
             @Override
             public void add(Widget widget) {
@@ -80,51 +81,51 @@ public class GwtRpcDemo implements EntryPoint {
         };
         RootPanel.get("logger").add(logPanel);
         Logger.getLogger("").addHandler(new HasWidgetsLogHandler(logPanel));
-        
-                
+
+
         RPCSerializer rpc_serializer = GWT.create(RPCSerializer.class);
-                
-        AtmosphereRequestConfig rpcRequestConfig = AtmosphereRequestConfig.create(rpc_serializer);
+
+        RequestConfig rpcRequestConfig = AtmosphereRequestConfig.create(rpc_serializer);
         rpcRequestConfig.setUrl(GWT.getModuleBaseURL() + "atmosphere/rpc");
         rpcRequestConfig.setTransport(AtmosphereRequestConfig.Transport.STREAMING);
         rpcRequestConfig.setFallbackTransport(AtmosphereRequestConfig.Transport.LONG_POLLING);
         rpcRequestConfig.setOpenHandler(new AtmosphereOpenHandler() {
             @Override
-            public void onOpen(AtmosphereResponse response) {
+            public void onOpen(AtmosphereServerResponse response) {
                 logger.info("RPC Connection opened");
             }
         });
         rpcRequestConfig.setReopenHandler(new AtmosphereReopenHandler() {
             @Override
-            public void onReopen(AtmosphereResponse response) {
+            public void onReopen(AtmosphereServerResponse response) {
                 logger.info("RPC Connection reopened");
             }
         });
         rpcRequestConfig.setCloseHandler(new AtmosphereCloseHandler() {
             @Override
-            public void onClose(AtmosphereResponse response) {
+            public void onClose(AtmosphereServerResponse response) {
                 logger.info("RPC Connection closed");
             }
         });
         rpcRequestConfig.setMessageHandler(new AtmosphereMessageHandler() {
             @Override
-            public void onMessage(AtmosphereResponse response) {
+            public void onMessage(AtmosphereServerResponse response) {
                List<RPCEvent> messages = response.getMessages();
                for (RPCEvent event : messages) {
                   logger.info("received message through RPC: " + event.getData());
                }
             }
         });
-        
-        // trackMessageLength is not required but makes the connection more robust, does not seem to work with 
+
+        // trackMessageLength is not required but makes the connection more robust, does not seem to work with
         // unicode characters
 //        rpcRequestConfig.setFlags(Flags.trackMessageLength);
-        
-        
-        
+
+
+
         Atmosphere atmosphere = Atmosphere.create();
-        final AtmosphereRequest rpcRequest = atmosphere.subscribe(rpcRequestConfig);
-        
+        final AtmosphereServerRequest rpcRequest = atmosphere.subscribe(rpcRequestConfig);
+
         sendRPC.addClickHandler(new ClickHandler() {
           @Override
           public void onClick(ClickEvent event) {
@@ -140,8 +141,8 @@ public class GwtRpcDemo implements EntryPoint {
             }
           }
         });
-        
-        
+
+
     }
 
 }
