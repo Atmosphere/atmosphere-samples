2542db2bf259c343d2f34df80216985a1229fe6e
volkdir <dirk.volkmar@gmx.de>
2014-03-12 14:27:47 +0100

    Some further formatting changes

--- extensions-samples/gwt/gwt20-rpc/src/main/java/org/atmosphere/samples/shared/BaseEvent.java
+++ extensions-samples/gwt/gwt20-rpc/src/main/java/org/atmosphere/samples/shared/BaseEvent.java
@@ -2,17 +2,16 @@ package org.atmosphere.samples.shared;
 
 import java.io.Serializable;
 
-public class BaseEvent implements Serializable
-{
-	/**
+public class BaseEvent implements Serializable {
+    /**
 	 * 
 	 */
-	private static final long	serialVersionUID	= 1L;
-	String eventyp;
-	@Override
-	public String toString()
-	{
-		return "BaseEvent [eventyp=" + eventyp + "]";
-	}
-	
+    private static final long serialVersionUID = 1L;
+    String eventyp;
+    
+    @Override
+    public String toString() {
+        return "BaseEvent [eventyp=" + eventyp + "]";
+    }
+    
 }
