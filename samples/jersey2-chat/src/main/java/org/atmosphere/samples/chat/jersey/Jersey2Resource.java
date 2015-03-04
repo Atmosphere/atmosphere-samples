f0ff054876afeb1b22d1de6d163c506b0a43fc81
jfarcand <jfarcand@apache.org>
2015-03-03 21:49:32 -0500

    Migrate the application to use atmosphere.js

--- samples/jersey2-chat/src/main/java/org/atmosphere/samples/chat/jersey/Jersey2Resource.java
+++ samples/jersey2-chat/src/main/java/org/atmosphere/samples/chat/jersey/Jersey2Resource.java
@@ -19,30 +19,56 @@ import org.atmosphere.client.TrackMessageSizeInterceptor;
 import org.atmosphere.config.service.AtmosphereService;
 import org.atmosphere.cpr.ApplicationConfig;
 import org.atmosphere.cpr.AtmosphereResource;
-import org.atmosphere.cpr.Broadcaster;
-import org.atmosphere.cpr.BroadcasterFactory;
+import org.atmosphere.cpr.AtmosphereResourceEvent;
+import org.atmosphere.cpr.AtmosphereResourceEventListenerAdapter;
 import org.atmosphere.interceptor.AtmosphereResourceLifecycleInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 
 /**
- * Extremely simple chat application supportiong WebSocket, Server Side-Events, Long-Polling and Streaming.
+ * Extremely simple chat application supporting WebSocket, Server Side-Events, Long-Polling and Streaming.
  *
  * @author Jeanfrancois Arcand
  */
 @Path("/")
 @AtmosphereService(
-        dispatch = false,
+        dispatch = true,
         interceptors = {AtmosphereResourceLifecycleInterceptor.class, TrackMessageSizeInterceptor.class},
         path = "/chat",
         servlet = "org.glassfish.jersey.servlet.ServletContainer")
 public class Jersey2Resource {
 
+    private final Logger logger = LoggerFactory.getLogger(Jersey2Resource.class);
+
     @Context
     private HttpServletRequest request;
+
+    @GET
+    public void configureAtmosphereResource() {
+        AtmosphereResource r = (AtmosphereResource) request.getAttribute(ApplicationConfig.ATMOSPHERE_RESOURCE);
+
+        if (r != null) {
+            r.addEventListener(new AtmosphereResourceEventListenerAdapter.OnDisconnect() {
+                @Override
+                public void onDisconnect(AtmosphereResourceEvent event) {
+                    if (event.isCancelled()) {
+                        logger.info("Browser {} unexpectedly disconnected", event.getResource().uuid());
+                    } else if (event.isClosedByClient()) {
+                        logger.info("Browser {} closed the connection", event.getResource().uuid());
+                    }
+                }
+            });
+        } else {
+            throw new IllegalStateException();
+        }
+    }
+
 
     /**
      * Echo the chat message. Jackson can clearly be used here, but for simplicity we just echo what we receive.
